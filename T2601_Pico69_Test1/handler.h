c7b5436f5c6f52e23482472a6af0e5c3c0964de2
infrapale <infrapale@gmail.com>
2026-02-08 12:39:45 +0200

    no smoke so far

---
+++ T2601_Pico69_Test1/handler.h
@@ -0,0 +1,24 @@
+#ifndef __HANDLER_H__
+#define __HANDLER_H__
+#define FIELD_LEN   8
+#define NBR_OF_NODES  4
+
+typedef enum
+{
+    NODE_TYPE_UNDEF = 0,
+    NODE_TYPE_PIR,
+    NODE_TYPE_DOOR,
+    NODE_TYPE_TEMPERATURE,
+    NODE_TYPE_NBR_OF,
+} node_type_et;
+
+
+void handler_initialize(void);
+
+bool handler_parse_msg(char *msg, int16_t rssi );
+
+//void handler_process_event(event_radio_msg_st ev):
+
+void handler_debug_print(void);
+
+#endif
